c0a4ffbb0f9965a0272349fde971b39ced506efc
arikankainen <60932647+arikankainen@users.noreply.github.com>
2020-07-01 10:06:01 +0300

    lisätty portti herokua varten

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ const server = new ApolloServer({
 });
 
 void server
-  .listen()
+  .listen({ port: process.env.PORT || 4000 })
   .then(({ url }) => {
     console.log(`Server ready at ${url}`);
   });
