3029e53b454369aa8f808c1b203d69c48d73f0f2
arikankainen <60932647+arikankainen@users.noreply.github.com>
2020-07-03 10:29:32 +0300

    jest asennettu ja konfiguroitu, lisätty ensimmäinen dummy-testi

---
+++ src/tests/first.test.ts
@@ -0,0 +1,5 @@
+test('test', () => {
+  const result = 1;
+
+  expect(result).toBe(1);
+});
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,22 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const MONGODB_URI = process.env.NODE_ENV === 'development' ? process.env.MONGODB_URI_DEV : process.env.MONGODB_URI_PROD;
+//export const MONGODB_URI = process.env.NODE_ENV === 'development' ? process.env.MONGODB_URI_DEV : process.env.MONGODB_URI_PROD;
 export const MONGODB_URI_TEST = process.env.MONGODB_URI_TEST;
 export const JWT_PRIVATE_KEY = process.env.JWT_PRIVATE_KEY;
+
+let env;
+
+switch (process.env.NODE_ENV) {
+case 'production':
+  env = process.env.MONGODB_URI_PROD;
+  break;
+case 'development':
+  env = process.env.MONGODB_URI_DEV;
+  break;
+case 'test':
+  env = process.env.MONGODB_URI_TEST;
+  break;
+}
+
+export const MONGODB_URI = env;
