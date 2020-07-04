3fde77463dbffafa4a11b29296743bc55b245272
arikankainen <60932647+arikankainen@users.noreply.github.com>
2020-07-04 19:23:21 +0300

    testejä korjattu, lisätty beforeEach

--- src/tests/integration.test.ts
+++ src/tests/integration.test.ts
@@ -1,5 +1,7 @@
 import { createTestClient } from 'apollo-server-testing';
 import gql from 'graphql-tag';
+import bcrypt from 'bcrypt';
+
 import { server, mongoose } from '..';
 import UserModel from '../models/user';
 
@@ -52,12 +54,45 @@ const GET_USER = gql`
   }
 `;
 
-beforeAll(async () => {
+const initialUsers = [
+  {
+    username: 'admin',
+    password: '12345',
+    email: 'admin@test.fi',
+    fullname: 'Administrator',
+    isAdmin: true
+  },
+  {
+    username: 'user',
+    password: '00000',
+    email: 'user@test.fi',
+    fullname: 'Normal User',
+    isAdmin: false
+  },
+  {
+    username: 'special',
+    password: '99999',
+    email: 'special@admin.fi',
+    fullname: 'Special User',
+    isAdmin: false
+  }
+];
+
+beforeEach(async () => {
   await UserModel.deleteMany({});
+
+  const userObjects = await Promise.all(initialUsers.map(async user => {
+    const saltRounds = 10;
+    const hash = await bcrypt.hash(user.password, saltRounds);
+    user = { ...user, password: hash };
+    return new UserModel(user);
+  }));
+
+  await UserModel.insertMany(userObjects);
 });
 
 describe('Mutations', () => {
-  it('createUser works and returns created user', async () => {
+  it('new user can be created', async () => {
     const { mutate } = createTestClient(server);
 
     const res = await mutate({ mutation: CREATE_USER, variables: {
@@ -75,49 +110,55 @@ describe('Mutations', () => {
         isAdmin: false
       }
     };
-
+    
     expect(res.data).toEqual(createdUserData);
   });
 
-  it('logged user receives token', async () => {
+  it('user can login and receives token', async () => {
     const { mutate } = createTestClient(server);
 
     const res = await mutate({ mutation: LOGIN, variables: {
-      username: 'testUser',
+      username: 'admin',
       password: '12345'
     }});
 
-    const loginData = res.data && res.data.login ? res.data.login as { token: string } : { token: '' };
-    expect(loginData.token).toHaveLength(177);
+    interface Data {
+      token: string
+    }
+
+    const loginData = res.data && res.data.login ? res.data.login as Data : { token: '' };
+    expect(loginData.token).toHaveLength(173);
   });
 });
 
 describe('Queries', () => {
-  it('lists added user', async () => {
+  it('lists all users', async () => {
     const { query } = createTestClient(server);
     const res = await query({ query: LIST_USERS });
     
-    const queriedUserData = {
-      listUsers: [
-        {
-          username: 'testUser',
-          email: 'test@test.fi',
-          fullname: 'Test User',
-          isAdmin: false
-        }
-      ]
-    };
-    
-    expect(res.data).toEqual(queriedUserData);
+    interface Data {
+      username: string,
+      email: string,
+      fullname: string,
+      isAdmin: boolean
+    }
+
+    const usersData = res.data && res.data.listUsers ? res.data.listUsers as Data[] : [];
+    expect(usersData).toHaveLength(3);
   });
 
   it('finds user by username', async () => {
     const { query } = createTestClient(server);
-    const res = await query({ query: GET_USER, variables: { username: 'testUser' } });
+    const res = await query({ query: GET_USER, variables: { username: 'admin' } });
 
-    const userData = res.data && res.data.getUser ? res.data.getUser as { id: string, fullname: string } : { id: '', fullname: '' };
+    interface Data {
+      id: string,
+      fullname: string
+    }
+
+    const userData = res.data && res.data.getUser ? res.data.getUser as Data : { id: '', fullname: '' };
     expect(userData.id).toHaveLength(24);
-    expect(userData.fullname).toBe('Test User');
+    expect(userData.fullname).toBe('Administrator');
   });
 });
 
