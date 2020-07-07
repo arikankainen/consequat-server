5bf2092fe44c9fa44785769a0ccd338ea7ffdec5
arikankainen <60932647+arikankainen@users.noreply.github.com>
2020-07-07 14:18:15 +0300

    eslintiin lisätty sääntö että switchin caset pitää indentoida

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -8,15 +8,15 @@ export const JWT_PRIVATE_KEY = process.env.JWT_PRIVATE_KEY;
 let env;
 
 switch (process.env.NODE_ENV) {
-case 'production':
-  env = process.env.MONGODB_URI_PROD;
-  break;
-case 'development':
-  env = process.env.MONGODB_URI_DEV;
-  break;
-case 'test':
-  env = process.env.MONGODB_URI_TEST;
-  break;
+  case 'production':
+    env = process.env.MONGODB_URI_PROD;
+    break;
+  case 'development':
+    env = process.env.MONGODB_URI_DEV;
+    break;
+  case 'test':
+    env = process.env.MONGODB_URI_TEST;
+    break;
 }
 
 export const MONGODB_URI = env;
